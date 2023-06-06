35d37a73ad93d5d005924eb401edc2ebeee74cad
Ian Ferreira <ian_ferreira93@hotmail.com>
2023-06-06 17:43:41 -0300

    feat: req 3

--- ting_file_management/file_process.py
+++ ting_file_management/file_process.py
@@ -1,5 +1,20 @@
+from .file_management import txt_importer
+import sys
+
+
 def process(path_file, instance):
-    """Aqui irá sua implementação"""
+    processed_data = {
+            "nome_do_arquivo": path_file,
+            "qtd_linhas": len(txt_importer(path_file)),
+            "linhas_do_arquivo": txt_importer(path_file)
+        }
+
+    for index in range(len(instance)):
+        if instance.search(index)["nome_do_arquivo"] == path_file:
+            return []
+
+    instance.enqueue(processed_data)
+    sys.stdout.write(str(processed_data))
 
 
 def remove(instance):
